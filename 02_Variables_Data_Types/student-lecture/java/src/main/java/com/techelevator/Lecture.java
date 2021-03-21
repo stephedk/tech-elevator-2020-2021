c190c57bb7b73dae8b49765f7eed53ca8869c468
dougkuchna <dougkuchna@gmail.com>
2021-03-21 12:05:09 -0400

    Add files via upload

---
+++ 02_Variables_Data_Types/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,146 @@
+package com.techelevator;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		
+		/*
+		1. Create a variable to hold an int and call it numberOfExercises.
+			Then set it to 26.
+		*/
+		int numberOfExercises;
+		numberOfExercises = 26;
+		System.out.println("1. numberOfExercises = " + numberOfExercises);
+
+		/*
+		2. Create a variable to hold a double and call it half.
+			Set it to 0.5.
+		*/
+		double half = 0.5;
+		System.out.println("2. half = " + half);
+
+		/*
+		3. Create a variable to hold a String and call it name.
+			Set it to "TechElevator".
+		*/
+		String name = "TechElevator";
+		System.out.println("3. name = " + name);
+
+		/*
+		4. Create a variable called seasonsOfFirefly and set it to 1.
+		*/
+		byte seasonsOfFirefly = 1;
+		System.out.println("4. seasonsOfFirefly = " + seasonsOfFirefly);
+
+		/*
+		5. Create a variable called myFavoriteLanguage and set it to "Java".
+		*/
+		String myFavoriteLanguage;
+		myFavoriteLanguage = "Java";
+		System.out.println("5. myFavoriteLanguage = " + myFavoriteLanguage);
+
+		/*
+		6. Create a variable called pi and set it to 3.1416.
+		*/
+		float pi = 3.1416f;
+		System.out.println("6. pi = " + pi);
+
+		/*
+		7. Create and set a variable that holds your name.
+		*/
+		String myName = "Stephen";
+		System.out.println("7. myName = " + myName);
+		/*
+		8. Create and set a variable that holds the number of buttons on your mouse.
+		*/
+		int numberOfButtonsOnMouse = 2;
+		System.out.println("8. numberOfButtonsOnMouse = " + numberOfButtonsOnMouse);
+		/*
+		9. Create and set a variable that holds the percentage of battery left on
+		your phone.
+		*/
+		
+		/*
+		10. Create an int variable that holds the difference between 121 and 27.
+		*/
+
+		/*
+		11. Create a double that holds the addition of 12.3 and 32.1.
+		*/
+
+		/*
+		12. Create a String that holds your full name.
+		*/
+
+		/*
+		13. Create a String that holds the word "Hello, " concatenated onto your
+		name from above.
+		*/
+
+		/*
+		14. Add a " Esquire" onto the end of your full name and save it back to
+		the same variable.
+		*/
+
+		/*
+		15. Now do the same as exercise 14, but use the += operator.
+		*/
+
+		/*
+		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
+		*/
+
+		/*
+		17. Add a 0 onto the end of the variable from exercise 16.
+		*/
+
+		/*
+		18. What is 4.4 divided by 2.2?
+		*/
+
+		/*
+		19. What is 5.4 divided by 2?
+		*/
+
+		/*
+		20. What is 5 divided by 2?
+		*/
+
+		/*
+		21. What is 5.0 divided by 2?
+		*/
+
+		/*
+		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
+		*/
+
+		/*
+		23. If I divide 5 by 2, what's my remainder?
+		*/
+		
+		int one23 = 5;
+		int two23 = 2;
+		int answer23 = one23 % two23; 
+		System.out.println("23. " + answer23);
+		// % returns the remainder
+		
+		/*
+		24. What is 1,000,000,000 * 3?
+		*/
+		
+		long billion = 100000000;
+		long threeBillion = billion * 3;
+		System.out.println("24. Three Billion= ");
+		
+		/*
+		25. Create a variable that holds a boolean called doneWithExercises and
+		set it to false.
+		*/
+		boolean doneWithExercises = false;
+		/*
+		26. Now set doneWithExercise to true.
+		*/
+		doneWithExdercises = true;
+	}
+
+}
